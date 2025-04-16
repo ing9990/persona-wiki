b8ce31c24c135324bd1e505c2b909b163d9dbb1a
ing9990 <mail80782258@gmail.com>
2025-04-16 09:01:52 +0900

    1. 사용하지 않는 코드 제거

--- storage-api/src/main/kotlin/io/ing9990/domain/comment/repository/querydsl/dto/ReplyResult.kt
+++
@@ -1,9 +0,0 @@
-package io.ing9990.domain.comment.repository.querydsl.dto
-
-data class ReplyResult(
-    val id: Long,
-    val content: String,
-    val likes: Int,
-    val dislikes: Int,
-    val userName: String?,
-)
